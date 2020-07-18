65f1d97c3e7177a5e1dd3ff1f6d8e8d59c696b3e
HimanshuBohra <39338459+HimanshuBohra@users.noreply.github.com>
2020-07-18 22:13:46 +0530

    Add files via upload
    First draft of INR exchange rate against EUR from 1 Jan 2019 to 31 Jan 2019 by using python library matplotlib

---
+++ T-1.py
@@ -0,0 +1,34 @@
+import json 
+import datetime
+import matplotlib.pyplot as plt
+# Opening JSON file 
+f = open('data.json','r') 
+  
+# returns JSON object as  
+# a dictionary 
+data = json.load(f) 
+  
+# Iterating through the json 
+# list 
+dates = []
+for i in data['rates']: 
+    dates.append(i)
+jan_dates = []
+for i in dates:
+    datee = datetime.datetime.strptime(i, "%Y-%m-%d")
+    if datee.month==1:
+        jan_dates.append(i)
+jan_dates.sort()
+INR_EUR_exchange = []
+for i in jan_dates:
+   INR_EUR_exchange.append(data['rates'][i]['INR'])
+plt.plot(jan_dates,INR_EUR_exchange)
+plt.xticks(rotation=90)
+plt.ylim(min(INR_EUR_exchange)-0.5,max(INR_EUR_exchange))
+
+plt.xlabel('January Dates')
+plt.ylabel('INR-EUR Exchange Rates')
+plt.title('INR exchange rate against EUR from 1 Jan 2019 to 31 Jan 2019')
+plt.show()
+# Closing file 
+f.close() 
